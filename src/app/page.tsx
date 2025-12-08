415e54aba877f4c2fe3e14fcd3d4280ef5932d34
AdnanM50 <a-19150@mangrove.edu.bd>
2025-12-08 15:54:04 +0600

    Add ServiceSection component and integrate it into the Home page
    - Created a new ServiceSection component to showcase various IT services.
    - Integrated ServiceSection into the Home page alongside existing sections.
    - Each service includes a title, description, button, and an icon for better visual representation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import AboutSection from "@/components/landing/about-section";
 import { HeroSection } from "@/components/landing/herosection";
+import ServiceSection from "@/components/landing/service-section";
 import TustedSection from "@/components/landing/tusted-section";
 
 export default function Home() {
   return (
-  <>
-<HeroSection />
-<TustedSection />
-<AboutSection />
-  </>
+    <>
+      <HeroSection />
+      <TustedSection />
+      <AboutSection />
+      <ServiceSection />
+    </>
   );
 }
