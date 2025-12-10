e6b46c137f8597c3e03e21738e04d97e8e406a19
AdnanM50 <a-19150@mangrove.edu.bd>
2025-12-10 15:45:14 +0600

    refactor: remove unused imports from CoreValuesSection component

--- src/app/about/_components/valuesection.tsx
+++ src/app/about/_components/valuesection.tsx
@@ -1,14 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import {
-  Lightbulb,
-  Users,
-  Trophy,
-  ShieldCheck,
-  Zap,
-  Handshake
-} from 'lucide-react';
+
 
 const CoreValuesSection = () => {
   const values = [
