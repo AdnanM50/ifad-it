8d3b3798d3642ec6cbbb4b62b3d2ba94bca9041b
AdnanM50 <a-19150@mangrove.edu.bd>
2025-12-14 00:09:21 +0600

    finish now

--- src/components/landing/featured.tsx
+++ src/components/landing/featured.tsx
@@ -15,25 +15,18 @@ const FeaturedOn: React.FC = () => {
     { name: "প্রথম আলো", src: "/download 1.png", alt: "Prothom Alo Logo" },
     { name: "The Daily Star", src: "/download (1) 1.png", alt: "The Daily Star Logo" },
     { name: "কালের কণ্ঠ", src: "/Link → kaler-kantho.png", alt: "Kaler Kontho Logo" },
-    { name: "Clutch", src: "/clutch-seeklogo 1.png", alt: "Clutch Logo" },
-    { name: "প্রথম আলো", src: "/download 1.png", alt: "Prothom Alo Logo" },
-    { name: "The Daily Star", src: "/download (1) 1.png", alt: "The Daily Star Logo" },
-    { name: "কালের কণ্ঠ", src: "/Link → kaler-kantho.png", alt: "Kaler Kontho Logo" },
-    { name: "Clutch", src: "/clutch-seeklogo 1.png", alt: "Clutch Logo" },
-    { name: "প্রথম আলো", src: "/download 1.png", alt: "Prothom Alo Logo" },
-    { name: "The Daily Star", src: "/download (1) 1.png", alt: "The Daily Star Logo" },
-    { name: "কালের কণ্ঠ", src: "/Link → kaler-kantho.png", alt: "Kaler Kontho Logo" },
+
   ];
 
   return (
-    <section className="py-12 md:py-[119px] px-2 lg:px-0 bg-white">
+    <section className="py-12 md:py-[119px] container px-2 lg:px-0 bg-white">
       <h2 className="section-subtitle text-center mb-8">Featured On</h2>
 
       {/* Marquee Wrapper */}
-      <div className="relative overflow-hidden">
-        <div className="flex w-max animate-marquee gap-8 md:gap-[110px]">
-          {[...logos, ...logos].map((logo, index) => (
-            <div key={index} className="flex items-center">
+      <div className="relative max-w-[899px] mx-auto overflow-hidden">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-8 md:gap-[110px]">
+          {[...logos].map((logo, index) => (
+            <div key={index} className="flex items-center justify-center">
               <Image
                 width={1000}
                 height={1000}
