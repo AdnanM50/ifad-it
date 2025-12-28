bdcc1d66930321ee2de33b4acc325f1faa8810ec
AdnanM50 <a-19150@mangrove.edu.bd>
2025-12-28 18:35:02 +0600

    create some new route

---
+++ src/app/service-software/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='mt-[85px] lg:mt-[100px]'>
+        Page</div>
+  )
+}
+
+export default Page
---
+++ src/app/service-tech/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+   <div className='mt-[85px] lg:mt-[100px]'>
+      
+    </div>
+  )
+}
+
+export default Page
